aed981afee1a9a2d25f50b035efe0c55d048b20b
xive30 <deflo59@gmail.com>
2023-06-08 16:01:54 +0200

    switch elephant

--- src/Database/sequelize.ts
+++ src/Database/sequelize.ts
@@ -3,23 +3,23 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE!,
-    process.env.DB_USERNAME!,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        port: 5432,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    });
+// const sequelize = new Sequelize(
+//     process.env.DB_DATABASE!,
+//     process.env.DB_USERNAME!,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST,
+//         dialect: 'postgres',
+//         port: 5432,
+//         pool: {
+//             max: 5,
+//             min: 0,
+//             acquire: 30000,
+//             idle: 10000
+//         }
+//     });
 
-// const sequelize = new Sequelize(process.env.URL_CONNECT!);
+const sequelize = new Sequelize(process.env.URL_CONNECT!);
 
     sequelize
 	.authenticate()
